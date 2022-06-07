23f75b3077d585ec1db74bf501c0a66a9335438c
jeroso <jeroso@nate.com>
2022-06-07 23:49:47 +0900

    엔티티 클래스 개발1

---
+++ src/main/java/jpabook/jpashop/domain/Delivary.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Delivary {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)    //ORDINAL : 숫자 , STRING : 문자 => 숫자를 사용 시 중간에 enum 추가시 에러발생.. 문자만 사용하자!
+    private DeliveryStatus status;  //READY, COMP
+}
