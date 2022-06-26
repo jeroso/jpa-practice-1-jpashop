6dcefd68d7f2277ca356581e15cb1cc00521d5ac
jeroso <jeroso@nate.com>
2022-06-26 22:42:56 +0900

    매핑 주의점

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Getter
 @Setter
@@ -16,11 +18,11 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "item_id")   //@JoinColumn => 연관관계 주인
     private Item item;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")  //@JoinColumn => 연관관계 주인
     private Order order;
 
