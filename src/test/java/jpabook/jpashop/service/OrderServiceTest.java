65c2e2c6b82fbdc1c579c68b2cabefd2addd3cca
jeroso <jeroso@nate.com>
2022-07-28 23:48:57 +0900

    주문서비스 테스트 완료

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -13,6 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 @Transactional
 class OrderServiceTest {
@@ -37,22 +39,11 @@ class OrderServiceTest {
         Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
         //then
         Order getOrder = orderRepository.findOne(orderId);
-        Assertions.assertEquals(OrderStatus.ORDER, getOrder.getStatus(), "상품 주문시 상태는 ORDER");
-        Assertions.assertEquals(1, getOrder.getOrderItems().size(), "주문한 상품 종류 수는 정확해야 한다.");
-        Assertions.assertEquals(10000 * orderCount, getOrder.getTotalPrice(), "주문 가격은 가격 * 수량 이다.");
+        assertEquals(OrderStatus.ORDER, getOrder.getStatus(), "상품 주문시 상태는 ORDER");
+        assertEquals(1, getOrder.getOrderItems().size(), "주문한 상품 종류 수는 정확해야 한다.");
+        assertEquals(10000 * orderCount, getOrder.getTotalPrice(), "주문 가격은 가격 * 수량 이다.");
     }
 
-
-    @Test
-    public void 주문취소() throws Exception {
-        //given
-
-        //when
-
-        //then
-
-    }
-    
     @Test
     public void 상품주문_재고수량초과() throws Exception {
 
@@ -66,9 +57,26 @@ class OrderServiceTest {
                 orderService.order(member.getId(), book.getId(), orderCount)
         );
         //then
-        Assertions.assertEquals("need more stock", thrown.getMessage());
+        assertEquals("need more stock", thrown.getMessage());
     }
 
+    @Test
+    public void 주문취소() throws Exception {
+        //given
+        Member member = createMember("홍길동");
+        Item item = createItem("시골 JPA", 10000, 10);
+
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        //when
+        orderService.cancelOrder(orderId);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+        assertEquals(OrderStatus.CANCEL, getOrder.getStatus(), "주문 취소시 상태는 CANCEL 이다.");
+        assertEquals(10, item.getStockQuantity(), "주문 취소된 상품은 그만큼 재고가 증가해야 한다.");
+    }
     private Item createItem(String name, int price, int quantity) {
 
         Item book = new Book();
